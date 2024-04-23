ff77547c6242efafdc818c6aa5cbe6d2958003c1
Myrrh Hsu <myrrhrachel.hsu@gmail.com>
2024-04-22 20:02:02 -0400

    new changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-5a3050aa-a295-4067-8ca2-6e6c9e7ad77e.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-f192444c-5e64-4643-9108-6c36a2415065.containerhub.tripleten-services.com"
 }
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -4,27 +4,43 @@ const config = require('../config');
 const requestBody = {
 	"productsList": [
         {
-            "id": 1,
-            "quantity": 2
-        },
-        {
-            "id": 6,
-            "quantity": 2
+            "id": 5,
+            "quantity": 1
         }
     ]
 }
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/1/products`, {
+test('status should be 200', async () => {
+    let actualStatus;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/2`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+		actualStatus = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatus).toBe(200);
 });
 
+
+test('adding items to the cart', async () => {
+    let actualResponseBody;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/2`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+        actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualResponseBody["wareHouse"]).toBe("Fresh Food");
+});
